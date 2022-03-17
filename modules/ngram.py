5e8c50c84acfdc65f1aa8ae80ea9184a4d7141a0
Cactiw <Knyazev1514@gmail.com>
2022-03-17 12:57:34 +0300

    Added start smooth

--- modules/ngram.py
+++ modules/ngram.py
@@ -1,3 +1,5 @@
+import itertools
+import logging
 from typing import List, Tuple
 
 from modules.dict_service import inc_dict_counter
@@ -10,6 +12,14 @@ def count_ngrams(tokens: List[str]) -> Tuple[dict]:
         inc_dict_counter(res_2, (word, word_1))
         inc_dict_counter(res_3, (word, word_1, word_2))
     return res, res_2, res_3
+
+
+def smooth(d1: dict, d2: dict, d3: dict):
+    for word_1, word_2 in itertools.combinations(d1, 2):
+        d2.update({(word_1, word_2): d2.get((word_1, word_2), 0) + 1})
+    logging.info("Started smooth trigrams")
+    for word_1, word_2, word_3 in itertools.combinations(d1, 3):
+        d3.update({(word_1, word_2, word_3): d2.get((word_1, word_2, word_3), 0) + 1})
 
 
 def count_perplection(prob: float, n: int):
--- modules/web_modules/add_file.py
+++ modules/web_modules/add_file.py
@@ -1,20 +1,25 @@
 import logging
+from typing import Optional
 
 from fastapi import UploadFile, File
 
 from modules.core.app import app
 from modules.dict_service import merge_dicts
-from modules.ngram import count_ngrams
+from modules.ngram import count_ngrams, smooth
 from modules.resources import Resources
 from modules.tokenizer import tokenize
 
 
 @app.post("/api/addFile")
-async def add_file(file: UploadFile = File(...)):
+async def add_file(file: UploadFile = File(...), use_smooth: Optional[bool] = False):
     logging.info('Loading file...')
     text = (await file.read()).decode()
     tokens = tokenize(text)
     d, d2, d3 = count_ngrams(tokens)
+
+    if use_smooth:
+        logging.info("Using smoothing!")
+        smooth(d, d2, d3)
 
     Resources.d_1 = merge_dicts(Resources.d_1, d)
     Resources.d_2 = merge_dicts(Resources.d_2, d2)
